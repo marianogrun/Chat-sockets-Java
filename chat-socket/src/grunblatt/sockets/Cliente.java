bf90dcfd27a1ba62d9ac5427f140b850e2f0ef5f
marianogrun <marianogrun@gmail.com>
2020-08-27 19:16:57 -0300

    Added events for socket and server communications. Still in progress

--- chat-socket/src/grunblatt/sockets/Cliente.java
+++ chat-socket/src/grunblatt/sockets/Cliente.java
@@ -1,6 +1,11 @@
 package grunblatt.sockets;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
 
 public class Cliente {
 
@@ -34,8 +39,34 @@ class LaminaMarcoCliente extends JPanel{
         campo1 = new JTextField(20);
         add(campo1);
         miBoton=new JButton("Enviar");
+
+        EnviaTexto mievento=new EnviaTexto();
+
+        miBoton.addActionListener(mievento);
+
         add(miBoton);
 
+    }
+
+    private class EnviaTexto implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //System.out.println(campo1.getText());
+
+            try {
+                Socket clientSocket =new Socket("localhost",9000);
+
+                DataOutputStream stream=new DataOutputStream(clientSocket.getOutputStream());
+
+                stream.writeUTF(campo1.getText());
+
+                stream.close();
+
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 
     private JTextField campo1;
--- chat-socket/src/grunblatt/sockets/Servidor.java
+++ chat-socket/src/grunblatt/sockets/Servidor.java
@@ -2,6 +2,12 @@ package grunblatt.sockets;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 
@@ -14,7 +20,7 @@ public class Servidor {
         }
 }
 
-class MarcoServidor extends JFrame {
+class MarcoServidor extends JFrame implements Runnable {
     private JTextArea areatexto;
 
     public MarcoServidor(){
@@ -31,6 +37,33 @@ class MarcoServidor extends JFrame {
         add(milamina);
 
         setVisible(true);
+
+        Thread mihilo=new Thread(this);
+
+        mihilo.start();
     }
+
+    @Override
+    public void run() {
+        //System.out.println("Estoy a la escucha");
+
+        try {
+            ServerSocket server= new ServerSocket(9000);
+
+            while(true) {
+                Socket socket = server.accept();
+
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+
+                String mensajeTexto = inputStream.readUTF();
+                areatexto.append("\n" + mensajeTexto);
+
+                socket.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
 

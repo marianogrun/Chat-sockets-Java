2aaab2b1991ccd95fd52ab2006bca58b84963874
sms-mgrunblatt <mgrunblatt@sms-latam.com>
2020-08-26 21:50:27 -0300

    first commit - Cliente y Servidor

---
+++ chat-socket/src/grunblatt/sockets/Servidor.java
@@ -0,0 +1,36 @@
+package grunblatt.sockets;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Servidor {
+
+        public static void main(String[] args){
+
+            MarcoServidor mimarco= new MarcoServidor();
+
+            mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        }
+}
+
+class MarcoServidor extends JFrame {
+    private JTextArea areatexto;
+
+    public MarcoServidor(){
+
+        setBounds(1200,300,280,350);
+        JPanel milamina = new JPanel();
+
+        milamina.setLayout(new BorderLayout());
+
+        areatexto= new JTextArea();
+
+        milamina.add(areatexto,BorderLayout.CENTER);
+
+        add(milamina);
+
+        setVisible(true);
+    }
+}
+
